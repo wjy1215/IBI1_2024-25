0a56b3ac788d331899056095028d02aa3c6fe42e
wjy1215 <jinyuan.24@intl.zju.edu.cn>
2025-05-14 14:11:14 +0800

    Update mystery_code.py

--- Practical 4/mystery_code.py
+++ Practical 4/mystery_code.py
@@ -4,10 +4,11 @@
 # Import libraries
 # randint allows drawing a random number,
 # e.g. randint(1,5) draws a number between 1 and 5
+
 from random import randint
 from math import ceil
 progress=0
-while progress>=0:
+while progress>=0:   
 	progress+=1
 	first_n = randint(1,6)
 	second_n = randint(1,6)
